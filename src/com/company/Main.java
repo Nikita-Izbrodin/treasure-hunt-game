216cd17e6573115ddd20e5d0ba70bf59bf194dfb
NI210248 <NI210248@reigate.ac.uk>
2021-11-09 16:15:01 +0000

    minor changed to board display

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[][] board = new int[4][4]; // 2D integer array with 4 rows and 4 columns
+        String[][] board = new String[4][4]; // 2D integer array with 4 rows and 4 columns
 
         Random random = new Random();
         int treasurePosX = random.nextInt(4);
         int treasurePosY = random.nextInt(4);
 
-        board[treasurePosX][treasurePosY] = treasurePosX+1;
+
+        board[treasurePosX][treasurePosY] = "$";
         /*
            0 0 0 0
            0 0 0 0
@@ -21,7 +22,10 @@ public class Main {
          */
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; ++j) {
+                if (board[i][j] != null)
                 System.out.print(board[i][j]);
+                // != $ == null
+                // == $ != null
             }
             System.out.println();
         }
