c2306268cc86270ebbeaf9420777e053f29c8301
NI210248 <NI210248@reigate.ac.uk>
2021-11-12 11:00:48 +0000

    added 2 of '$' to the board, working on the input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class Main {
 
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 	// write your code here
         String[][] board = new String[4][4]; // 2D integer array with 4 rows and 4 columns
@@ -11,8 +13,11 @@ public class Main {
         Random random = new Random();
         int treasurePosX = random.nextInt(4);
         int treasurePosY = random.nextInt(4);
+        board[treasurePosX][treasurePosY] = "$";
 
+        treasurePosX = random.nextInt(4);
 
+        treasurePosY = random.nextInt(4);
         board[treasurePosX][treasurePosY] = "$";
         /*
            0 0 0 0
@@ -20,14 +25,26 @@ public class Main {
            0 0 0 0
            0 0 0 0
          */
+        System.out.println("   1  2  3  4");
         for (int i = 0; i < board.length; i++) {
+            System.out.print(i+1 + " ");
             for (int j = 0; j < board.length; ++j) {
-                if (board[i][j] != null)
-                System.out.print(board[i][j]);
+                if (board[i][j] != null) {
+                    System.out.printf("[%s]", board[i][j]);
+                }
+                else {
+                    System.out.print("[ ]");
+                }
                 // != $ == null
                 // == $ != null
             }
             System.out.println();
         }
+
+        while (true) {
+            int userInp = input.nextInt();
+
+        }
+
     }
 }
